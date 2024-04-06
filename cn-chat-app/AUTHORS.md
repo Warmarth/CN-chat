b6aa2125e6b9dd6a43b3b39269093f98e69ad88e
Ibeaka Godson <godsonibeaka@yahoo.com>
2024-04-05 18:22:45 -0700

    daissy UI installed

---
+++ cn-chat-app/AUTHORS.md
@@ -0,0 +1,6 @@
+# Authors
+
+| Name          | Email                 |
+| ------------- | --------------------- |
+| Cobby Franky      | godsonibeaka@gmail.com  |
+| Jane Smith    | janesmith@example.com |
--- cn-chat-app/src/App.jsx
+++ cn-chat-app/src/App.jsx
@@ -1,4 +1,8 @@
-
 export default function App() {
-  return <h1 className="text-3xl text-center font-bold underline">Cobby and Nehal chat app</h1>;
+  return (
+    <div className="text-center">
+      <h1 className="text-3xl font-bold underline">Cobby and Nehal chat app</h1>
+      <button class="btn btn-accent btn-outline">Button</button>
+    </div>
+  );
 }
